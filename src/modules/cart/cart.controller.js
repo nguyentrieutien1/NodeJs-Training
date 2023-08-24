6fa63f084f4cbecd23998ee275c8f890015f668b
nguyentrieutien1 <nguyenthanhtung111xxx@gmail.com>
2023-08-24 16:28:50 +0700

    Revert "D" d This reverts commit 625ab74533d6a4ea8c380c49d8555535cc6a07ee.

---
+++ src/modules/cart/cart.controller.js
@@ -0,0 +1,63 @@
+const { Ok, Success } = require("../../core/success.response");
+const { errorHandler } = require("../../helpers/handleError");
+const cartService = require("./cart.service");
+class CartController {
+  findAll = async (req, res) => {
+    try {
+      const products = await cartService.findAll();
+      return new Ok({
+        data: products,
+        message: "Get all product successful !",
+      }).send(res);
+    } catch (error) {
+      errorHandler(error, res);
+    }
+  };
+  create = async (req, res) => {
+    try {
+      const payload = req.body;
+      const product = await cartService.create({ payload });
+      return new Success({
+        message: "Product has been created !",
+        data: product,
+      }).send(res);
+    } catch (error) {
+      errorHandler(error, res);
+    }
+  };
+  findOneById = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const product = await cartService.findOneById({ id });
+      return new Ok({ data: product }).send(res);
+    } catch (error) {
+      errorHandler(error, res);
+    }
+  };
+  findOneAndUpdate = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const payload = req.body;
+      const product = await cartService.findOneAndUpdate({
+        id,
+        payload,
+      });
+      return new Ok({
+        data: product,
+        message: "Update product successful !",
+      }).send(res);
+    } catch (error) {
+      errorHandler(error, res);
+    }
+  };
+  findOneAndDelete = async (req, res) => {
+    try {
+      const { id } = req.params;
+      await cartService.findOneAndDelete({ id });
+      return new Ok({ message: "Delete product successful !" }).send(res);
+    } catch (error) {
+      errorHandler(error, res);
+    }
+  };
+}
+module.exports = new CartController();
---
+++ src/modules/cart/cart.service.js
@@ -0,0 +1,40 @@
+const { FETCH_TIME_OUT } = require("../../contains/fetchTimeout");
+const { getDbData } = require("../../helpers/getDataDb");
+const { sleep } = require("../../helpers/sleep");
+const { BadRequestError, NotFound } = require("../../core/error.response");
+const { Cart } = require("./cart.model");
+class CartService {
+  findAll = async () => {
+    const cart = await Cart.find();
+    return cart;
+  };
+  create = async ({ payload }) => {
+    if (!payload) {
+      throw new BadRequestError("Dont't have payload !", { payload });
+    }
+    const cartItem = await Cart.create(payload);
+    return cartItem;
+  };
+  findOneById = async ({ id }) => {
+    if (!id) throw new BadRequestError("Missing cart item id !", { id });
+    const cartItem = await Cart.findOne({ $where: id });
+    if (!cartItem) throw new NotFound("Cart item not found !", { id });
+    return cartItem;
+  };
+  findOneAndUpdate = async ({ id, payload }) => {
+    if (!id || !payload)
+      throw new BadRequestError("Missing cart item or payload !", {
+        id,
+        payload,
+      });
+    const cartItem = await Cart.findOneAndUpdate({ $where: id }, payload);
+    return cartItem;
+  };
+  findOneAndDelete = async ({ id }) => {
+    if (!id)
+      throw new BadRequestError("Missing cart item  id or payload !", { id });
+    await Cart.findOneAndDelete({ $where: id });
+    return 1;
+  };
+}
+module.exports = new CartService();
